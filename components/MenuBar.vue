5ca9554fe07b3fae645fda08e8bc66d2fec06ba3
Jorge Parra <jorgeparraandrade@gmail.com>
2019-04-08 22:51:23 -0400

    Responsive menu ready

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -19,7 +19,27 @@
 				</ul>
 			</div>
 
+
 		</div>
+
+		<div class="resp-menu">
+			<img class="logo" src="../assets/img/logo.png" alt="Berlin Baqueira">
+			<img class="openmenu" src="../assets/img/menu.png" alt="Menu" ref="menutrigger" @click="openmenu()">
+		</div>
+
+		<div class="resp-menu-options" ref="respmenu">
+			<img class="close-btn" src="../assets/img/close.png" alt="close" ref="closetrigger" @click="closemenu()">
+			<ul>
+				<li><a href="#" v-scroll-to="'#top'" @click="closemenu()">Inicio</a></li>
+				<li><a href="#" v-scroll-to="'#nosotros'" @click="closemenu()">Nosotros</a></li>
+				<li><a href="#" v-scroll-to="'#empresas'" @click="closemenu()">Empresas</a></li>
+				<li><a href="#" v-scroll-to="'#modelo'" @click="closemenu()">Modelo</a></li>
+				<li><a href="#" v-scroll-to="'#proyectos'" @click="closemenu()">Proyectos</a></li>
+				<li><a href="#" v-scroll-to="'#contacto'" @click="closemenu()">Contacto</a></li>
+			</ul>
+		</div>
+
+
 
 	</div>
 
@@ -27,7 +47,15 @@
 
 <script>
 	export default {
-			
+			methods:{
+				openmenu(){
+					this.$refs.respmenu.style.transform = 'translateY(0)';
+				},
+
+				closemenu(){
+					this.$refs.respmenu.style.transform = 'translateY(-100vh)';
+				}
+			}
 	}
 </script>
 
@@ -51,6 +79,10 @@
       flex-direction: row;
       align-items: flex-start;
       justify-content: space-between;
+
+			@media (max-width: 1080px) {
+				display: none;
+			}
 		}
 
 		.main-logo{
@@ -58,6 +90,9 @@
 			img{
 				width:100%;
 				margin-top: -20px;
+			}
+			@media (max-width: 1080px) {
+				display: none;
 			}
 		}
 
@@ -86,7 +121,82 @@
 				}
 
 			}
+
+			@media (max-width: 1080px) {
+				display: none;
+			}
 		}
+		@media (max-width: 1080px) {
+			padding: 0;
+		}
+	}
+
+	.resp-menu{
+
+		display: none;
+
+		@media (max-width:1080px){
+			width: 100%;
+			background-color: #ffffff;
+			height: 81px;
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 18px 0 37px;
+			position: fixed;
+
+			.logo{
+				width: 162px;
+				height: 36.21px;
+				cursor: pointer;
+			}
+
+		}
+	}
+
+	.openmenu{
+		cursor: pointer;
+	}
+
+	.resp-menu-options{
+		position: absolute;
+    background-color: #ffffff;
+    width: 100%;
+		height: 100vh;
+		position: fixed;
+		display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+		transform: translateY(-100vh);
+		transition: all ease .3s;
+
+		ul{
+
+			list-style: none;
+
+			li{
+				list-style: none;
+				padding: 10px 0;
+    		font-size: 20px;
+				text-align: center;
+
+				a{
+					color: inherit;
+					text-decoration: none;
+				}
+
+			}
+
+		}
+	}
+
+	.close-btn{
+		position: absolute;
+    top: 20px;
+    right: 20px;
+		cursor: pointer;
 	}
 
 </style>
